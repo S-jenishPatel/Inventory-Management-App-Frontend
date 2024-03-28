1a83939c1daac8cf869f44364d8f3a82719c761c
S-jenishPatel <sjenishpatel23@gmail.com>
2024-03-28 17:44:08 +0530

    add header to add-product and back button to product page

--- src/Add Product/AddProduct.jsx
+++ src/Add Product/AddProduct.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import Axios from "axios";
 import toast from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
+import Header from "../Header/Header";
 
 import "./AddProduct.styles.css";
 import warningImage from "../assets/warning.png";
@@ -80,113 +81,116 @@ function AddProduct() {
   };
 
   return (
-    <div className="add-product">
-      <fieldset>
-        <legend>Add Product</legend>
+    <div className="add-product-container">
+      <Header />
+      <div className="add-product">
+        <fieldset>
+          <legend>Add Product</legend>
 
-        <div id="product-image-uploader">
-          <label htmlFor="product-image">Upload Product Image:</label>
-          <input
-            type="file"
-            id="product-image"
-            onChange={(e) => {
-              setImage(e.target.files[0]);
-              console.log(e.target.files[0]);
-            }}
-          />
-        </div>
-        {image ? (
-          <img
-            src={URL.createObjectURL(image)}
-            className="product-image"
-            alt="Product"
-          />
-        ) : null}
-
-        <div className="add-product-input-container">
-          <label htmlFor="product-name">Enter Product Name :</label>
-          <input
-            type="text"
-            id="product-name"
-            className="add-product-input"
-            value={name}
-            onChange={(e) => {
-              setName(e.target.value);
-            }}
-          />
-
-          <label htmlFor="product-category">Enter Product Category :</label>
-          <input
-            type="text"
-            id="product-category"
-            className="add-product-input"
-            value={category}
-            onChange={(e) => {
-              setCategory(e.target.value);
-            }}
-            list="product-categories"
-          />
-          <datalist id="product-categories">
-            {categories?.map((category, index) => {
-              return <option key={index} value={category.name} />;
-            })}
-          </datalist>
-
-          <label htmlFor="product-price">Enter Product Price :</label>
-          <input
-            type="number"
-            id="product-price"
-            className="add-product-input"
-            value={price}
-            onChange={(e) => {
-              setPrice(e.target.value);
-            }}
-          />
-
-          <label htmlFor="product-quantity">Enter Product Quantity :</label>
-          <input
-            type="number"
-            id="product-quantity"
-            className="add-product-input"
-            value={quantity}
-            onChange={(e) => {
-              setQuantity(e.target.value);
-            }}
-          />
-
-          <label htmlFor="product-description">
-            Enter Product Description :
-          </label>
-          <textarea
-            id="product-description"
-            className="add-product-input"
-            cols="30"
-            rows="5"
-            value={description}
-            onChange={(e) => {
-              setDescription(e.target.value);
-            }}
-          />
-        </div>
-
-        {/* Landing error text */}
-        {errorText ? (
-          <div id="api-error-div">
-            <img src={warningImage} alt="Warning Image" />
-            <span>{errorText}</span>
+          <div id="product-image-uploader">
+            <label htmlFor="product-image">Upload Product Image:</label>
+            <input
+              type="file"
+              id="product-image"
+              onChange={(e) => {
+                setImage(e.target.files[0]);
+                console.log(e.target.files[0]);
+              }}
+            />
           </div>
-        ) : null}
+          {image ? (
+            <img
+              src={URL.createObjectURL(image)}
+              className="product-image"
+              alt="Product"
+            />
+          ) : null}
 
-        <button
-          type="submit"
-          onClick={(e) => {
-            e.preventDefault();
-            handleAddProduct();
-          }}
-        >
-          Add Product
-        </button>
-      </fieldset>
+          <div className="add-product-input-container">
+            <label htmlFor="product-name">Enter Product Name :</label>
+            <input
+              type="text"
+              id="product-name"
+              className="add-product-input"
+              value={name}
+              onChange={(e) => {
+                setName(e.target.value);
+              }}
+            />
+
+            <label htmlFor="product-category">Enter Product Category :</label>
+            <input
+              type="text"
+              id="product-category"
+              className="add-product-input"
+              value={category}
+              onChange={(e) => {
+                setCategory(e.target.value);
+              }}
+              list="product-categories"
+            />
+            <datalist id="product-categories">
+              {categories?.map((category, index) => {
+                return <option key={index} value={category.name} />;
+              })}
+            </datalist>
+
+            <label htmlFor="product-price">Enter Product Price :</label>
+            <input
+              type="number"
+              id="product-price"
+              className="add-product-input"
+              value={price}
+              onChange={(e) => {
+                setPrice(e.target.value);
+              }}
+            />
+
+            <label htmlFor="product-quantity">Enter Product Quantity :</label>
+            <input
+              type="number"
+              id="product-quantity"
+              className="add-product-input"
+              value={quantity}
+              onChange={(e) => {
+                setQuantity(e.target.value);
+              }}
+            />
+
+            <label htmlFor="product-description">
+              Enter Product Description :
+            </label>
+            <textarea
+              id="product-description"
+              className="add-product-input"
+              cols="30"
+              rows="5"
+              value={description}
+              onChange={(e) => {
+                setDescription(e.target.value);
+              }}
+            />
+          </div>
+
+          {/* Landing error text */}
+          {errorText ? (
+            <div id="api-error-div">
+              <img src={warningImage} alt="Warning Image" />
+              <span>{errorText}</span>
+            </div>
+          ) : null}
+
+          <button
+            type="submit"
+            onClick={(e) => {
+              e.preventDefault();
+              handleAddProduct();
+            }}
+          >
+            Add Product
+          </button>
+        </fieldset>
+      </div>
     </div>
   );
 }
--- src/Product/Product.jsx
+++ src/Product/Product.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import Header from "../Header/Header";
 import EditProduct from "./EditProduct";
 
@@ -8,6 +8,9 @@ import "./Product.styles.css";
 function Product() {
   const { state } = useLocation();
   const [editProduct, setEditProduct] = useState(false);
+
+  const navigate = useNavigate();
+
   return (
     <div className="product">
       <Header />
@@ -15,48 +18,58 @@ function Product() {
       {editProduct ? (
         <EditProduct setEditProduct={setEditProduct} product={state} />
       ) : (
-        <div className="product-details-container">
-          <div className="product-details-image">
-            <img src={state.image} alt={state.name} />
-          </div>
-          <div className="product-details">
-            <h4>{state.name}</h4>
-            <div className="product-details-category">
-              <p>{state.categoryName}</p>
-              <span
-                style={{
-                  backgroundColor:
-                    state.quantity > 0 ? "forestgreen" : "crimson",
+        <>
+          <button
+            onClick={(e) => {
+              e.preventDefault();
+              navigate("/app/home");
+            }}
+          >
+            &lt; Back
+          </button>
+          <div className="product-details-container">
+            <div className="product-details-image">
+              <img src={state.image} alt={state.name} />
+            </div>
+            <div className="product-details">
+              <h4>{state.name}</h4>
+              <div className="product-details-category">
+                <p>{state.categoryName}</p>
+                <span
+                  style={{
+                    backgroundColor:
+                      state.quantity > 0 ? "forestgreen" : "crimson",
+                  }}
+                >
+                  {state.quantity > 0 ? "In Stock" : "Out of Stock"}
+                </span>
+              </div>
+              <div className="product-subdetails">
+                <p>
+                  <span>Price :</span> ${state.price.toFixed(2)}
+                </p>
+                <p>
+                  <span>Qauntity :</span> {state.quantity}
+                </p>
+                <p>
+                  <span>Total Value in stock :</span> $
+                  {(state.price * state.quantity).toFixed(2)}
+                </p>
+                <p>
+                  <span>Description :</span> {state.description}
+                </p>
+              </div>
+              <button
+                onClick={(e) => {
+                  e.preventDefault();
+                  setEditProduct(true);
                 }}
               >
-                {state.quantity > 0 ? "In Stock" : "Out of Stock"}
-              </span>
+                Edit Product
+              </button>
             </div>
-            <div className="product-subdetails">
-              <p>
-                <span>Price :</span> ${state.price.toFixed(2)}
-              </p>
-              <p>
-                <span>Qauntity :</span> {state.quantity}
-              </p>
-              <p>
-                <span>Total Value in stock :</span> $
-                {(state.price * state.quantity).toFixed(2)}
-              </p>
-              <p>
-                <span>Description :</span> {state.description}
-              </p>
-            </div>
-            <button
-              onClick={(e) => {
-                e.preventDefault();
-                setEditProduct(true);
-              }}
-            >
-              Edit Product
-            </button>
           </div>
-        </div>
+        </>
       )}
     </div>
   );
